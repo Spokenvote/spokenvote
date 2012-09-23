00d94b224d0833ce1847a23851a7f0519a26362c
Gabriel Williams <ummonai@gmail.com>
2012-09-23 12:02:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spokenvote::Application.config.secret_token = '910e9b08d227cb486f4d2fa6411cd78ce40374f4ec2d73894a7c88800857f324bced5f576194506e5203dbd4cecafe7a37cda1388afe17bc4e116d4ee856f83d'
