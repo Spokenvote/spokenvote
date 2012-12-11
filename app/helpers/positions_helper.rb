94a29969956d3d5709c3c9751fb7bfcced81ca8f
billsaysthis <blazar@gmail.com>
2012-12-11 09:38:49 -0800

    pause to merge from upstream

--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -1,2 +1,6 @@
 module PositionsHelper
+  def position_title(total_votes, username)
+    title_text = (total_votes > 0) ? 'Proposal by ' : 'Improvement by '
+    content_tag(:h4, raw('&mdash; ') + title_text + username)
+  end
 end
