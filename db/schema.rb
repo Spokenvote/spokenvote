898b5e02e39f9012701fcd74140a5cea422182ad
Pratik Khadloya <tispratik@gmail.com>
2012-12-14 00:11:33 -0800

    New tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121214075248) do
+ActiveRecord::Schema.define(:version => 20121214080916) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(:version => 20121214075248) do
     t.string   "location"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.integer  "type"
+    t.integer  "parent_id"
+    t.string   "ancestry"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "proposals", :force => true do |t|
@@ -67,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20121214075248) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "location_id"
   end
 
 end
