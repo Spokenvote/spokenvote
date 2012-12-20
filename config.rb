523b429fea261c01b303eeb9d1c3b19cfb66d1c2
billsaysthis <blazar@gmail.com>
2012-12-20 14:47:52 -0800

    switch to vwall/compass-twitter-bootstrap from bootstrap-sass and use fontAwesome instead of glyphicons plus removed unneeded app/assets/sass folder

--- config.rb
+++ config.rb
@@ -1,10 +1,9 @@
 # Require any additional compass plugins here.
-require 'bootstrap-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "app/assets/stylesheets"
-sass_dir = "app/assets/sass"
+sass_dir = "app/assets/stylesheets"
 images_dir = "app/assets/images"
 javascripts_dir = "app/assets/javascripts"
 
