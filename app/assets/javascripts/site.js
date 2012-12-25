9c468b611ef06d9c48f1363a05a7f736e1b8f297
Bill Lazar <blazar@gmail.com>
2012-12-24 21:24:08 -0800

    new homepage with viewport.js for height, plus styling of locate hub

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -9,4 +9,8 @@ $(function() {
   $('#closeHero').click(hider);
   $('select').select2();
   $('.related_supporting').last().css('border-bottom', 'none')
+  if ($('section.span11').length > 0) {
+    vp = new Viewport();
+    $('section.span11').height(vp.height);
+  }
 })
