cdf49be4ec124218fda571e133bdd9062ba90a84
Pratik Khadloya <tispratik@gmail.com>
2012-12-26 21:14:37 -0800

    Trying to fix the heroku problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ Spokenvote::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.assets.initialize_on_precompile = false # To fix https://github.com/Compass/compass-rails/issues/19 https://github.com/spree/spree_fancy/issues/2
 end
