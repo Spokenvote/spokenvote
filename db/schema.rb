c5a3c95b1c295f240b08019e8580c1760d82c28b
Kim Miller <kimardenmiller@gmail.com>
2012-12-31 11:27:26 -0800

    Allow all related porposals to show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121220040803) do
+ActiveRecord::Schema.define(:version => 20121227035256) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -76,8 +76,8 @@ ActiveRecord::Schema.define(:version => 20121220040803) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.integer  "location_id"
     t.integer  "hub_id"
+    t.string   "ip_address"
   end
 
 end
