45d48b430023e4cd904c485dc79484561366448b
Bill Lazar <blazar@gmail.com>
2013-01-06 15:07:46 -0800

    initial model, db, UI support for location association with Vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -12,14 +12,14 @@
 #
 
 class Vote < ActiveRecord::Base
-  attr_accessible :comment, :user, :proposal, :user_id, :proposal_id, :hub_id, :hub, :ip_address
+  attr_accessible :comment, :user, :proposal, :user_id, :proposal_id, :hub_id, :hub, :ip_address, :location
 
   # Associations
   belongs_to :proposal, :counter_cache => true
   belongs_to :user
   belongs_to :hub
+  belongs_to :location
 
-  validates :comment, :user_id, :proposal_id, :presence => true
+  validates :comment, :user_id, :proposal_id, :location_id, :presence => true
   validates :user_id, :uniqueness => {:scope => [:user_id, :proposal_id], :message => "Can't vote on the same issue twice."}
-
 end
