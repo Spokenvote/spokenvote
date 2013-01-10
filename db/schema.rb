5a968bb498ba89f04e1629f0d152db276aa69e0b
Bill Lazar <blazar@gmail.com>
2013-01-09 21:22:14 -0800

    edit support for improve and support buttons, cancel works but not save

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109222200) do
+ActiveRecord::Schema.define(:version => 20130110032648) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20130109222200) do
 
   create_table "locations", :force => true do |t|
     t.string   "name"
-    t.integer  "type"
+    t.integer  "type_id"
     t.integer  "parent_id"
     t.string   "ancestry"
     t.datetime "created_at", :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 begin
   i = 0
   hubs = ['Hacker Dojo','Marriage Equality','Net Neutrality','NHL','Solar Power']
-  geos = ['Mountain View','San Antonio','California','Texas','USA']
+  # geos = ['Mountain View','San Antonio','California','Texas','USA']
+
+  usa = Location.create({type_id: 1, name: 'USA'})
+  ca = Location.create({type_id: 2, name: 'CA', parent_id: usa.id})
+  tx = Location.create({type_id: 2, name: 'TX', parent_id: usa.id})
+  mv = Location.create({type_id: 3, name: 'Mountain View', parent_id: ca.id})
+  sa = Location.create({type_id: 3, name: 'San Antonio', parent_id: tx.id})
 
   5.times do
-    hubs << Hub.create({group: hubs[i], description: Faker::Lorem.sentence})
+    hubs << Hub.create({group: hubs[i], location: Location.all.sample, description: Faker::Lorem.sentence})
     i += 1
   end
 
   users = []
+  # let's create a standard known user for simplicity sake
+  users << User.create({name: 'Voter1', email: 'voter1@example.com', password: 'abc123', password_confirmation: 'abc123'})
   5.times do
     users << User.create({name: Faker::Name.name, email: Faker::Internet.email, password: 'abc123', password_confirmation: 'abc123'})
   end
