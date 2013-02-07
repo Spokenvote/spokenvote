4df35a3007e5e7ffc9e47710686b09c7bf1f6f3f
Kim Miller <kimardenmiller@gmail.com>
2013-02-07 11:56:05 -0800

    Forgot password email format improvements and correct attachment of support@spokenvote.org to the system  "from" address.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "kimardenmiller@gmail.com"
+  config.mailer_sender = "support@spokenvote.org"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
