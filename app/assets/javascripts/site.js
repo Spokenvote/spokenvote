231469cf1c57acfc26ef00750c5ca3b99eeb789a
Bill Lazar <blazar@gmail.com>
2013-02-08 10:08:05 -0800

    fixes footer positioning

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -99,8 +99,12 @@ window.app = {};
 
   app.setPageHeight = function() {
     var vp = new Viewport(), vph = vp.height;
-    if (!($('section.clear').length > 0 || $('section.searched').length > 0) && (vph > $('#mainContent').height())) {
-      $('#mainContent').height(vph - 140);
+    if ($('section.clear').length > 0 || $('section.searched').length > 0) {
+      $('#mainContent').height(vph - 142);
+    } else {
+      if (vph > $('#mainContent').height()) {
+        $('#mainContent').height(vph - 140);
+      }
     }
   }
 
