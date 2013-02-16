8ef5424e3e05059a816701ac230e0e1a849105fb
Pratik Khadloya <tispratik@gmail.com>
2013-02-15 23:56:59 -0800

    Added Active Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def authenticate_admin_user!
+    redirect_to new_user_session_path unless current_user.try(:is_admin?)
+  end
 end
