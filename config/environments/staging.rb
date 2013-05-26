9752111170a2b35ddd07311b1e542d7ec25bea55
Kim Miller <kimardenmiller@gmail.com>
2013-05-26 16:46:30 -0700

    Expermimenting with config.assets.compress set to TRUE

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ Spokenvote::Application.configure do
   config.serve_static_assets = true # Make it true for heroku
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
