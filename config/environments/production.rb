3ae5b746e54838e9cb6863e2b4a5454352f82909
kimardenmiller <kimardenmiller@gmail.com>
2014-01-13 17:50:21 -0800

    Set Procfile and puma.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,9 @@ Spokenvote::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'spokenvote.org' }
+
   # Enable threaded mode
-  # config.threadsafe!
+  config.threadsafe!
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
