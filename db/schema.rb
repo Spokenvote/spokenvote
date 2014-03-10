62e35823f1b0c105b9d7a2ccf5ce7f1055f47225
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-10 15:23:17 -0700

    Added preferences hstore to the User model, to allow for the storage of notification preferences

--- db/schema.rb
+++ db/schema.rb
@@ -9,92 +9,116 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130722013752) do
+ActiveRecord::Schema.define(version: 20140310194542) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
     t.string   "author_type"
     t.text     "body"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "namespace"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
-  create_table "authentications", :force => true do |t|
+  create_table "activities", force: true do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "notify_list",    default: [], array: true
+  end
+
+  add_index "activities", ["notify_list"], name: "index_activities_on_notify_list", using: :gin
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
+
+  create_table "authentications", force: true do |t|
     t.integer  "user_id"
-    t.string   "uid",        :null => false
-    t.string   "provider",   :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "uid",        null: false
+    t.string   "provider",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "token"
   end
 
-  add_index "authentications", ["user_id"], :name => "index_authentications_on_user_id"
+  add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
 
-  create_table "hubs", :force => true do |t|
+  create_table "hubs", force: true do |t|
     t.string   "group_name"
     t.text     "description"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "location_id"
     t.string   "formatted_location"
   end
 
-  add_index "hubs", ["formatted_location", "group_name"], :name => "index_hubs_on_formatted_location_and_group_name", :unique => true
+  add_index "hubs", ["formatted_location", "group_name"], name: "index_hubs_on_formatted_location_and_group_name", unique: true, using: :btree
 
-  create_table "proposals", :force => true do |t|
+  create_table "proposals", force: true do |t|
     t.string   "statement"
     t.integer  "user_id"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.integer  "votes_count", :default => 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "votes_count", default: 0
     t.string   "ancestry"
     t.integer  "created_by"
     t.integer  "hub_id"
   end
 
-  add_index "proposals", ["ancestry"], :name => "index_positions_on_ancestry"
-  add_index "proposals", ["hub_id"], :name => "index_proposals_on_hub_id"
-  add_index "proposals", ["user_id"], :name => "index_proposals_on_user_id"
+  add_index "proposals", ["ancestry"], name: "index_proposals_on_ancestry", using: :btree
+  add_index "proposals", ["hub_id"], name: "index_proposals_on_hub_id", using: :btree
+  add_index "proposals", ["user_id"], name: "index_proposals_on_user_id", using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => ""
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: ""
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
+    t.hstore   "preferences"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "votes", :force => true do |t|
+  create_table "votes", force: true do |t|
     t.integer  "proposal_id"
     t.text     "comment"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "ip_address"
   end
 
-  add_index "votes", ["proposal_id", "user_id"], :name => "index_votes_on_proposal_id_and_user_id"
-  add_index "votes", ["proposal_id"], :name => "index_votes_on_proposal_id"
-  add_index "votes", ["user_id"], :name => "index_votes_on_user_id"
+  add_index "votes", ["proposal_id", "user_id"], name: "index_votes_on_proposal_id_and_user_id", using: :btree
+  add_index "votes", ["proposal_id"], name: "index_votes_on_proposal_id", using: :btree
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
 
 end
