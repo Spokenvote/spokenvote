6270f0ea5c40978ce9cfc405f42fd87850354a88
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-23 16:10:22 -0700

    Results of the Rails generate mailer voter_mailer command. Mailer, views and spec created.

---
+++ app/mailers/voter_mailer.rb
@@ -0,0 +1,3 @@
+class VoterMailer < ActionMailer::Base
+  default from: "from@example.com"
+end
---
+++ spec/mailers/voter_mailer_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe VoterMailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
