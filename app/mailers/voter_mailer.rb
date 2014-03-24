211523a9439b4335eb7ffc41f5639d87b0c3ca22
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-23 18:13:58 -0700

    Created the vote_notification action in the VoterMailer
    It will pass a User object representing the Recipient and an array of Vote objects representing the votes that the recipient is meant to be notified about.
    
    I've included the attachment as specified in the previous mailer branch
    https://github.com/railsforcharity/spokenvote/pull/269/files

--- app/mailers/voter_mailer.rb
+++ app/mailers/voter_mailer.rb
@@ -1,3 +1,11 @@
 class VoterMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "donotreply@spokenvote.org"
+
+  def vote_notification(user_notifications)
+    recipient_id = user_notifications.keys[0]
+    @recipient = User.find(recipient_id)
+    @votes = user_notifications[recipient_id].map { |vote_id| Vote.find(vote_id) }
+    attachments["bluefull.png"] = File.read("#{Rails.root}/app/assets/images/bluefull.png")
+    mail(:to => "#{@recipient.name} <#{@recipient.email}>", :subject => "People have voted on proposals you are interested in!")
+  end
 end
