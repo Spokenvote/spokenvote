263e56a25627a31c4f731ad371a386ff2c888433
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-23 23:27:03 -0700

    Fixed up the VoterMailer action #vote_notification to work with a different datashape for the argument. Moved from expecting a hash to expecting two separate arguments (an integer and an array of integers)
    Commented out the attachment since it's not currently being used but left line as reference for later implementation

--- app/mailers/voter_mailer.rb
+++ app/mailers/voter_mailer.rb
@@ -1,11 +1,10 @@
 class VoterMailer < ActionMailer::Base
   default from: "donotreply@spokenvote.org"
 
-  def vote_notification(user_notifications)
-    recipient_id = user_notifications.keys[0]
-    @recipient = User.find(recipient_id)
-    @votes = user_notifications[recipient_id].map { |vote_id| Vote.find(vote_id) }
-    attachments["bluefull.png"] = File.read("#{Rails.root}/app/assets/images/bluefull.png")
+  def vote_notification(user_id, vote_array)
+    @recipient = User.find(user_id)
+    @votes = vote_array.map { |vote_id| Vote.find(vote_id) }
+    # attachments["bluefull.png"] = File.read("#{Rails.root}/app/assets/images/bluefull.png")
     mail(:to => "#{@recipient.name} <#{@recipient.email}>", :subject => "[Spokenvote] People have voted on topics you are interested in!")
   end
 end
