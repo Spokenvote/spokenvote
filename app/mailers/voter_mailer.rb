e0535e37d634d66f1437c7abdca8257d4e367c07
kimardenmiller <kimardenmiller@gmail.com>
2014-04-10 17:12:19 -0700

    Reformatted from

--- app/mailers/voter_mailer.rb
+++ app/mailers/voter_mailer.rb
@@ -1,10 +1,10 @@
 class VoterMailer < ActionMailer::Base
-  default from: "donotreply@spokenvote.org"
+  default from: 'Spokenvote <donotreply@spokenvote.org>'
 
   def vote_notification(user_id, vote_array)
     @recipient = User.find(user_id)
     @votes = vote_array.map { |vote_id| Vote.find(vote_id) }
     # attachments["bluefull.png"] = File.read("#{Rails.root}/app/assets/images/bluefull.png")
-    mail(:to => "#{@recipient.name} <#{@recipient.email}>", :subject => "[Spokenvote] People have voted on topics you are interested in!")
+    mail(to: "#{@recipient.name} <#{@recipient.email}>", subject: 'New votes have arrived on your topics!')
   end
 end
