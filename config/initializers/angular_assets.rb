a966dd1ec370a0b97400a09de3eff1aad58663ac
kimardenmiller <kimardenmiller@gmail.com>
2014-04-15 13:32:58 -0700

    Set Slim attr_delims

--- config/initializers/angular_assets.rb
+++ config/initializers/angular_assets.rb
@@ -2,3 +2,5 @@ Rails.application.assets.register_engine '.haml', Tilt::HamlTemplate
 
 Rails.application.assets.register_engine '.slim', Slim::Template
 
+Slim::Engine.set_default_options attr_delims: { '(' => ')', '[' => ']' }
+
