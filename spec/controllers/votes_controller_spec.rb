3db7aa8e7a95388d29afc47e30d46151b44f7f4a
kimardenmiller <kimardenmiller@gmail.com>
2014-04-28 18:56:15 -0700

    Tests appear working with old Rails 4.0.3 bug

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -21,33 +21,32 @@ describe VotesController do
         expect {
           post :create, vote: { proposal_id: proposal1.id, comment: "Coz it rocks!"}
         }.to change(Vote, :count).by(1)
-        proposal = Proposal.find_by_id(assigns(:vote).proposal_id)
-        proposal.votes_count.should == 2
+        assigns(:vote).user.should == user1
+        assigns(:vote).proposal.should == proposal1
+        Proposal.find_by_id(proposal1.id).votes_count.should == 2
       end
     end
 
-    context 'current user has one vote in the proposal tree and tries to vote again' do
+    context 'current user has vote already on this proposal' do
       let!(:proposal2) { create(:proposal, user: user1, parent: proposal1, hub: hub) }
       let!(:vote2) { create(:vote, proposal: proposal2, user: user1) }
 
-      it 'does not create a new vote' do
+      it 'should not create a new vote' do
         expect {
           post :create, vote: { proposal_id: proposal2.id, comment: "Coz this modified proposal rocks better!"}
         }.to change(Vote, :count).by(0)
-        proposal2 = Proposal.find_by_id(assigns(:vote).proposal_id)
-        proposal2.votes_count.should == 1
+        Proposal.find_by_id(proposal1.id).votes_count.should == 1
+        Proposal.find_by_id(proposal2.id).votes_count.should == 1
       end
 
-      it 'moves the vote from the previous proposal to proposal1' do
+      it 'moves the vote from the proposal2 to proposal1' do
         expect {
           post :create, vote: { proposal_id: proposal1.id, comment: "Changed my mind, the original one rocks better!"}
         }.to change(Vote, :count).by(0)
         assigns(:vote).user.should == user1
         assigns(:vote).proposal.should == proposal1
-        proposal1 = Proposal.find_by_id(assigns(:vote).proposal_id)
-        # proposal1.votes_count.should == 1
-
-        # assigns(:proposal1).votes_count.should == 2
+        Proposal.find_by_id(proposal1.id).votes_count.should == 2
+        Proposal.find_by_id(proposal2.id).votes_count.should == 0
       end
     end
   end
