71e5d6834eba7badf2d4f2c55248a551b548faeb
kimardenmiller <kimardenmiller@gmail.com>
2014-04-30 12:37:15 -0700

    Revised votes_controller spec to use clearer current_user

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -9,7 +9,8 @@ describe VotesController do
   end
 
   let(:hub) { create(:hub) }
-  let!(:user1) { user }
+  # let!(:user1) { user }
+  let(:current_user) { user } # Logged in user
   let!(:user2) { create(:user) }
   let!(:proposal1) { create(:proposal, user: user2, hub: hub) }
   let!(:vote1) { create(:vote, proposal: proposal1, user: user2) }
@@ -21,15 +22,15 @@ describe VotesController do
         expect {
           post :create, vote: { proposal_id: proposal1.id, comment: "Coz it rocks!"}
         }.to change(Vote, :count).by(1)
-        assigns(:vote).user.should == user1
+        assigns(:vote).user.should == current_user
         assigns(:vote).proposal.should == proposal1
         Proposal.find_by_id(proposal1.id).votes_count.should == 2
       end
     end
 
     context 'current user has vote already on this proposal' do
-      let!(:proposal2) { create(:proposal, user: user1, parent: proposal1, hub: hub) }
-      let!(:vote2) { create(:vote, proposal: proposal2, user: user1) }
+      let!(:proposal2) { create(:proposal, user: current_user, parent: proposal1, hub: hub) }
+      let!(:vote2) { create(:vote, proposal: proposal2, user: current_user) }
 
       it 'should not create a new vote' do
         expect {
@@ -43,10 +44,10 @@ describe VotesController do
         expect {
           post :create, vote: { proposal_id: proposal1.id, comment: "Changed my mind, the original one rocks better!"}
         }.to change(Vote, :count).by(0)
-        assigns(:vote).user.should == user1
+        assigns(:vote).user.should == current_user
         assigns(:vote).proposal.should == proposal1
         # Proposal.find_by_id(proposal1.id).votes_count.should == 2
-        # Proposal.find_by_id(proposal2.id).votes_count.should == 0
+        Proposal.find_by_id(proposal2.id).votes_count.should == 0
       end
     end
   end
