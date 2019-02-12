3faceba6e0b148f9fe951172cdb09b637dce1762
kimardenmiller <kimardenmiller@gmail.com>
2019-02-12 13:26:02 -0800

    - Upgraded actionpack (>= 4.2.5.1) - Removed non-working, old Google search lookup code

--- spec/services/google_places_autocomplete_service_spec.rb
+++ spec/services/google_places_autocomplete_service_spec.rb
@@ -16,41 +16,41 @@ describe GooglePlacesAutocompleteService do
 			end
 		end
 
-		context "city search" do 
-			if $API_KEY_DEFINED
-				it "locates and identifies the city" do 
-					locations = @service.find_regions("Mountain View")
-					expect(locations[0][:description]).to eq("Mountain View, CA, United States")
-					expect(locations[0][:id]).to eq("bb51f066ff3fd0b033db94b4e6172da84b8ae111")
-					expect(locations[0][:type]).to eq("City of")
-					expect(locations[0][:reference]).to_not be_nil
-				end
-			end
-		end 
+		# context "city search" do       # Feb 12, 2019 believe lookup is in Angular now
+		# 	if $API_KEY_DEFINED
+		# 		it "locates and identifies the city" do
+		# 			locations = @service.find_regions("Mountain View")
+		# 			expect(locations[0][:description]).to eq("Mountain View, CA, United States")
+		# 			expect(locations[0][:id]).to eq("bb51f066ff3fd0b033db94b4e6172da84b8ae111")
+		# 			expect(locations[0][:type]).to eq("City of")
+		# 			expect(locations[0][:reference]).to_not be_nil
+		# 		end
+		# 	end
+		# end
 
-		context "county search" do 
-			if $API_KEY_DEFINED
-				it "locates and identifies the county" do 
-					locations = @service.find_regions("Santa Clara County")
-					expect(locations[0][:description]).to eq("Santa Clara County, CA, United States")
-					expect(locations[0][:id]).to eq("1019a381cf34eb999ba02adcfeaf3f066d662fb1")
-					expect(locations[0][:type]).to eq("County of")
-					expect(locations[0][:reference]).to_not be_nil
-				end
-			end
-		end 
+		# context "county search" do         # Feb 12, 2019 believe lookup is in Angular now
+		# 	if $API_KEY_DEFINED
+		# 		it "locates and identifies the county" do
+		# 			locations = @service.find_regions("Santa Clara County")
+		# 			expect(locations[0][:description]).to eq("Santa Clara County, CA, United States")
+		# 			expect(locations[0][:id]).to eq("1019a381cf34eb999ba02adcfeaf3f066d662fb1")
+		# 			expect(locations[0][:type]).to eq("County of")
+		# 			expect(locations[0][:reference]).to_not be_nil
+		# 		end
+		# 	end
+		# end
 
-		context "state search" do 
-			if $API_KEY_DEFINED
-				it "locates and identifies the state" do 
-					locations = @service.find_regions("California")
-					expect(locations[0][:description]).to eq("California, United States")
-					expect(locations[0][:id]).to eq("b993df4055071925c7950dc2afd22c0b98f994e0")
-					expect(locations[0][:type]).to eq("State of")
-					expect(locations[0][:reference]).to_not be_nil
-				end
-			end
-		end
+		# context "state search" do   # Feb 12, 2019 believe lookup is in Angular now
+		# 	if $API_KEY_DEFINED
+		# 		it "locates and identifies the state" do
+		# 			locations = @service.find_regions("California")
+		# 			expect(locations[0][:description]).to eq("California, United States")
+		# 			expect(locations[0][:id]).to eq("b993df4055071925c7950dc2afd22c0b98f994e0")
+		# 			expect(locations[0][:type]).to eq("State of")
+		# 			expect(locations[0][:reference]).to_not be_nil
+		# 		end
+		# 	end
+		# end
 
 		context "country search" do 
 			if $API_KEY_DEFINED
@@ -76,17 +76,17 @@ describe GooglePlacesAutocompleteService do
 		# 	end
 		# end
 
-    context "new district search" do
-			if $API_KEY_DEFINED
-				it "locates and does not return entry" do
-					locations = @service.find_regions("Hampton Beach")
-					expect(locations[0][:description]).to eq("Hampton Beach, Hampton, NH, United States")
-					expect(locations[0][:id]).to eq("5f59aebc53bba8ff82b7dc98ea629977e58ebaf8")
-					expect(locations[0][:type]).to eq("District of")
-					expect(locations[0][:reference]).to_not be_nil
-				end
-			end
-		end
+    # context "new district search" do     # Feb 12, 2019 believe lookup is in Angular now
+		# 	if $API_KEY_DEFINED
+		# 		it "locates and does not return entry" do
+		# 			locations = @service.find_regions("Hampton Beach")
+		# 			expect(locations[0][:description]).to eq("Hampton Beach, Hampton, NH, United States")
+		# 			expect(locations[0][:id]).to eq("5f59aebc53bba8ff82b7dc98ea629977e58ebaf8")
+		# 			expect(locations[0][:type]).to eq("District of")
+		# 			expect(locations[0][:reference]).to_not be_nil
+		# 		end
+		# 	end
+		# end
 
   end
 
